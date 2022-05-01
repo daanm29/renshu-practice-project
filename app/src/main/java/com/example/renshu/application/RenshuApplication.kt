d488e93085e311d7ca1c2d0b8d7ae35e08be3007
Daan Molendijk <demolen2912@gmail.com>
2022-05-01 17:36:40 +0200

    Added Dagger-Hilt and created Application class

---
+++ app/src/main/java/com/example/renshu/application/RenshuApplication.kt
@@ -0,0 +1,9 @@
+package com.example.renshu.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RenshuApplication : Application() {
+    
+}
