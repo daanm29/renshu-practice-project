cb1074acdf2839f7cd800e0737b609bd2c7c1d43
Daan Molendijk <demolen2912@gmail.com>
2022-05-17 20:13:43 +0200

    Fixed an issue relating the splashscreen and the app not being able to open.

--- app/src/main/java/com/example/renshu/splash/SplashActivity.kt
+++ app/src/main/java/com/example/renshu/splash/SplashActivity.kt
@@ -3,7 +3,10 @@ package com.example.renshu.splash
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.example.renshu.R
 import com.example.renshu.main.MainActivity
 
 @SuppressLint("CustomSplashScreen")
@@ -11,8 +14,14 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        navigateMain()
+    }
 
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+    private fun navigateMain() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
     }
 }
